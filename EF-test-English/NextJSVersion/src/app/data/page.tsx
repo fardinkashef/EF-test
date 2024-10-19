b816fb28a478d03738e8bfce8fbdc18e36d421b6
Fardin Kashef <fardinkashef1397@gmail.com>
2024-10-19 21:37:17 +0330

    Next.js version: Enabling force dynamic

--- EF-test-English/NextJSVersion/src/app/data/page.tsx
+++ EF-test-English/NextJSVersion/src/app/data/page.tsx
@@ -1,6 +1,6 @@
 import SubjectList from "@/components/SubjectList";
 import { getSubjects } from "@/lib/server-actions/subjects";
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export default async function Data() {
   const subjects = await getSubjects();
